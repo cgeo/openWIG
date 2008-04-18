3c9fd19e59f4573246488398c2fe42891f26c7bd
matejcik <matejcik@df40d42f-224a-0410-a5e2-759f519755f9>
2008-04-18 02:48:48 +0000

    - dummy support for PositionAccuracy
    git-svn-id: https://openwig.googlecode.com/svn/trunk@69 df40d42f-224a-0410-a5e2-759f519755f9

--- src/openwig/Player.java
+++ src/openwig/Player.java
@@ -5,9 +5,23 @@ import se.krka.kahlua.vm.*;
 public class Player extends Container {
 	public ZonePoint position;
 	
+	private static class Method implements JavaFunction {
+		public int call(LuaCallFrame callFrame, int nArguments) {
+			callFrame.push(new Double(1));
+			return 1;
+		}
+	}
+	
+	private static Method positionAccuracy_GetValue = new Method();
+	
 	public static void register (LuaState state) {
 		EventTable.register(state);
 		state.setUserdataMetatable(Player.class, metatable);
+	}
+	
+	public Player() {
+		table.rawset("PositionAccuracy",this);
+		table.rawset("GetValue", positionAccuracy_GetValue);
 	}
 	
 	public int visibleThings() {
