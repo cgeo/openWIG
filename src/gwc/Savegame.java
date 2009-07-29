09faca6c073b34c4bab95ccb9bf43fe5e13ec8c4
matejcik <matejcik@df40d42f-224a-0410-a5e2-759f519755f9>
2009-07-29 01:08:12 +0000

    executive code for saving and loading; now we need an infrastructure to actually use it
    git-svn-id: https://openwig.googlecode.com/svn/trunk@270 df40d42f-224a-0410-a5e2-759f519755f9

---
+++ src/gwc/Savegame.java
@@ -0,0 +1,42 @@
+package gwc;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.DataOutputStream;
+import javax.microedition.io.file.FileConnection;
+import openwig.LuaInterface;
+import se.krka.kahlua.vm.LuaTable;
+
+public class Savegame {
+
+	private static final String SIGNATURE = "openWIG savegame\n";
+	
+	private FileConnection saveFile;
+
+	public Savegame (FileConnection fc) {
+		fc = saveFile;
+	}
+
+	public void store (LuaTable table)
+	throws IOException {
+		saveFile.truncate(0);
+		DataOutputStream out = saveFile.openDataOutputStream();
+
+		out.writeUTF(SIGNATURE);
+		LuaInterface.serializeLuaTable(table, out);
+	}
+
+	public void restore (LuaTable table)
+	throws IOException {
+		DataInputStream dis = saveFile.openDataInputStream();
+		String sig = dis.readUTF();
+		if (!SIGNATURE.equals(sig)) {
+			throw new IOException("Invalid savegame file: bad signature.");
+		}
+		try {
+			LuaInterface.deserializeLuaTable(dis, table);
+		} catch (IOException e) {
+			throw new IOException("Problem loading game: "+e.getMessage());
+		}
+	}
+}
