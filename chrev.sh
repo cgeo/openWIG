3d64b7f32db1711f10b07b23ce168a472ed3bb89
matejcik <matejcik@df40d42f-224a-0410-a5e2-759f519755f9>
2010-01-18 00:11:39 +0000

    fixed chrev script
    git-svn-id: https://openwig.googlecode.com/svn/trunk@312 df40d42f-224a-0410-a5e2-759f519755f9

--- chrev.sh
+++ chrev.sh
@@ -5,5 +5,5 @@ REVISION=`svnversion`
 FILES="src/openwig/Engine.java src/gui/Midlet.java"
 
 for i in $FILES; do
-	sed -i s/##REV##/$REVISION/ $i
+	sed -i "s/String VERSION = \".*\";/String VERSION = \"$REVISION\"/" $i
 done
