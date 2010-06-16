71b2462c3712c05195d26fe339a02f4948a4e984
matejcik <matejcik@df40d42f-224a-0410-a5e2-759f519755f9>
2010-06-16 20:50:10 +0000

    bwhahahahahahaaaa
    git-svn-id: https://openwig.googlecode.com/svn/trunk@381 df40d42f-224a-0410-a5e2-759f519755f9

--- chrev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REVISION=`svnversion`
-
-FILES="src/openwig/Engine.java src/gui/Midlet.java"
-
-for i in $FILES; do
-	sed -i "s/String VERSION = \".*\";/String VERSION = \"$REVISION\";/" $i
-done
