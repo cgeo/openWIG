f047b58f32552f4227b407c87a00c69c0bf00a3b
matejcik <ja@matejcik.cz>
2014-04-21 15:31:01 +0200

    license headers for OpenWIGLibrary

--- OpenWIGLibrary/src/cz/matejcik/openwig/Engine.java
+++ OpenWIGLibrary/src/cz/matejcik/openwig/Engine.java
@@ -1,3 +1,10 @@
+/* 
+ * Copyright (C) 2014 matejcik
+ *
+ * This program is covered by the GNU General Public License version 3 or any later version.
+ * You can find the full license text at <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * No express or implied warranty of any kind is provided.
+ */
 package cz.matejcik.openwig;
 
 import cz.matejcik.openwig.formats.*;
