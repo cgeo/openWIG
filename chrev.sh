1e49afd1f014962179d2932fb07863925971796c
Bernd <32555742+bekuno@users.noreply.github.com>
2020-04-18 00:54:29 +0200

    remove not OpenWIGLibrary content (#12)
    as discussed in https://github.com/cgeo/WhereYouGo/issues/42#issuecomment-596403673
    other parts are outdated and should removed:
    - DesktopWIG
    - droidwig
    - mobile
    - OpenWIGTester
    - chrev (revision changer for svn)
    
    The saveAnalyser needs a check if it is usable.

--- chrev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REVISION=`svnversion`
-
-FILES="OpenWIGLibrary/src/cz/matejcik/openwig/Engine.java mobile/src/gui/Midlet.java"
-
-for i in $FILES; do
-	sed -i "s/String VERSION = \".*\";/String VERSION = \"$REVISION\";/" $i
-done
