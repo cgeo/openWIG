db65b8af32d7ff5b48958bd8cb480364d25a7c41
Lars <gc@droescher.eu>
2020-08-11 12:04:06 +0200

    Update bug reporting hint

--- OpenWIGLibrary/src/cz/matejcik/openwig/Engine.java
+++ OpenWIGLibrary/src/cz/matejcik/openwig/Engine.java
@@ -241,7 +241,7 @@ public class Engine implements Runnable {
 			msg = e.toString();
 		}
 		log(msg, LOG_ERROR);
-		ui.showError("you hit a bug! please report at openwig.googlecode.com and i'll fix it for you!\n"+msg);
+		ui.showError("You hit a bug! Please report this to whereyougo@cgeo.org with a screenshot of this message.\n"+msg);
 	}
 
 	/** stops Engine */
